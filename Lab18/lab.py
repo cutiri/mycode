f97dced5f789ac1040ee667a80bfb3f63b4ccae7
cutiri <avillasante@gmail.com>
2023-10-30 20:53:54 +0000

    join and split

---
+++ Lab18/lab.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+
+    username = input("Please enter your name: ")
+    day = input("Please enter the day of the week: ")
+    
+    
+    # display the input back to the user.
+    print("Hello, ", username, "! Happy ", day)
+    
+# this calls main
+main()
