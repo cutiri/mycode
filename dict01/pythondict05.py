b5350e98436702a0fa788e9f9282bc78cb227a34
cutiri <avillasante@gmail.com>
2023-10-31 20:57:58 +0000

    learning about dictionaries

---
+++ dict01/pythondict05.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+def main():
+    dict = {
+        "name": "Darth Vader",
+        "real_name": "Anakin Skywalker",
+        "species": "Human",
+        "homeworld": "Tatooine",
+        "affiliation": "Sith",
+        "spouse": "Padmé Amidala",
+        "children": ["Luke Skywalker", "Leia Organa"],
+        "masters": ["Obi-Wan Kenobi (Jedi)", "Darth Sidious (Sith)"],
+        "apprentices": {
+            "Jedi": ["Ahsoka Tano"],
+            "Sith": ["Galen Marek / Starkiller", "Shira Brie / Lumiya"]
+        },
+        "weapon": "Red Lightsaber",
+    }
+
+    print("Choose a key of the list: ")
+    print(dict.keys())
+
+    userinput = input()
+
+    print(dict.get(userinput))
+
+    userinput_key = input("Enter a new key: ")
+    userinput_value = input("Enter a new value: ")
+
+    dict[userinput_key] = userinput_value
+
+    print("Dictionary has a new key/value: " + userinput_key + " - " + dict[userinput_key])
+
+
+main()
---
+++ dr-brown.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+dict = {
+    "full_name": "Emmett Lathrop Brown",
+    "alias": ["Emmett Brown", "Dr. Emmett Brown", "Doc Brown"],
+    "nickname": "Doc",
+    "home": "Hill Valley, California",
+    "spouse": "Clara Clayton Brown"
+}
+
+
+print(dict)
