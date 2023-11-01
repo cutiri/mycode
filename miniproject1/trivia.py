ccd302b1f582caaf403b7ad6c3a9d43d33c6c61f
Angel Villasante <avillasante@gmail.com>
2023-11-01 13:50:19 -0700

    trivia

--- miniproject1/trivia.py
+++ miniproject1/trivia.py
@@ -1,44 +1,66 @@
 import random, starcraft, question
 
 questions = starcraft.trivia_pool()
+#rightanswers = 0
+correctanswers = 0
+totalanswers = 0
+totalquestions = len(questions)
 
-right_answers = 0
-total_answers = 0
-total_questions = len(questions)
+def final_results():
+    percent = correctanswers / totalquestions * 100
+    final_comment = "Good job!!!" if (percent > 80) else "Try harder Dummy!!!"
+    print("-----------------------------------------------------------------------")
+    print("Congratulations, you completed the trivia.")
+    print("Correct answers:", correctanswers, ", out of", totalquestions)
+    print("Your success rate was: ", percent, "%. ", final_comment)
+    print("Bye!")
+    print("-----------------------------------------------------------------------")
 
+def print_question(question):
+    print("-----------------------------------------------------------------------")
+    print(question.question)
+    count = 1
+    for option in question.options:
+        print(count, ": ", option)
+        count += 1
+    print("-----------------------------------------------------------------------")
 
-print("Welcome to Trivia")
-
-
-while True:
-    print("Questions left: ", len(questions))
-    print("Right answers: ", right_answers)
-    question_number = random.randint(0, len(questions) - 1)
-    question = questions[question_number]
+def main():
+    global correctanswers
+    global totalanswers
+    global totalquestions
+    print("Welcome to Trivia")
 
     while True:
-        print(question.question)
+        print("Questions left: ", len(questions))
+        print("Right answers: ", correctanswers)
+        question_number = random.randint(0, len(questions) - 1)
+        question = questions[question_number]
 
-        count = 1
-        for option in question.options:
-            print(count, ": ", option)
-            count += 1
+        while True:
+            print_question(question)
 
-        user_input = input("Enter your answer: ")
+            try:
+                user_input = int(input("Enter your answer: "))
+                if not user_input in range(1, len(question.options) + 1):
+                    raise Exception()
+                if int(user_input) - 1 in question.right:
+                    print("Right answer!")
+                    correctanswers += 1
+                else:
+                    print("Wrong, you silly goose!")
 
-        if int(user_input) - 1 in question.right:
-            print("Right answer!")
-            right_answers += 1
-        else:
-            print("Wrong, you silly goose!")
-
-        total_answers += 1
-        break
+                totalanswers += 1
+                break
+            except Exception as e:
+                print("I see you can't follow instructions, try again.")
 
 
-    del questions[question_number]
-    if len(questions) == 0:
-        print("Congratulations, you completed the trivia.")
-        print("Total number of right answers: ", right_answers, ", of a total of: ", total_questions, ". For a success rate of: ", right_answers / total_questions * 100, "%")
-        print("Bye!")
-        break
+        del questions[question_number]
+        if len(questions) == 0:
+            final_results()
+            break
+
+
+if __name__ == "__main__":
+    main()
