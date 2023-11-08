4db5e4973bac4fb5b33dbd387c4c7f8519b7b511
Angel Villasante <avillasante@gmail.com>
2023-11-08 07:25:38 -0800

    trivia

--- trivia/main.py
+++ trivia/main.py
@@ -8,11 +8,9 @@ import random
 
 URL= "https://opentdb.com/api.php?amount=10&category=15"
 
-
 def display_question(question):
     pprint.pprint(html.unescape(question.get('question')))
     if question.get("type") == 'multiple':
-        pprint.pprint('Select the correct answer:')
         correct_answer_position = random.randint(0, len(question.get("incorrect_answers")))
         answers = question.get("incorrect_answers")
         answers.insert(correct_answer_position, question.get("correct_answer"))
@@ -20,8 +18,17 @@ def display_question(question):
         for answer in answers:
             pprint.pprint(str(count) + ": " + html.unescape(answer))
             count += 1
+        answer = int(input('Select the correct answer:'))
+        if answers[answer - 1] == question.get('correct_answer'):
+            print("The answer is correct!!!")
+        else:
+            print("The answer is incorrect.", "Correct answer is: ", question.get('correct_answer'))
     elif question.get("type") == 'boolean':     
-        pprint.pprint('Answer T or F:')
+        response = input('Answer T or F:').upper()
+        if (response == 'F' and question.get('correct_answer') == 'False') or (response == 'T' and question.get('correct_answer') == 'True'):
+            print("The answer is correct!!!")
+        else:
+            print("The answer is incorrect.")
         #pprint.pprint(html.unescape(question.get('question')))
 
 def main():
