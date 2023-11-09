349c2f831c6d5e1e6ff791856202d54022b81815
Angel Villasante <avillasante@gmail.com>
2023-11-09 11:37:35 -0800

    Lab119

--- miniprojectFlask/db.py
+++ miniprojectFlask/db.py
@@ -1,35 +1,46 @@
 import sqlite3
 
-# Connect to SQLite database (this will create a new database if it doesn't exist)
-conn = sqlite3.connect('starcraft.db')
+class DB:
+    def __init__(self) -> None:
+        self.conn = sqlite3.connect('starcraft.db')
+        pass
 
-# Create a cursor object to execute SQL queries
-cursor = conn.cursor()
+    def connect(self):
+        return sqlite3.connect('starcraft.db')
+    
+    def getCursor(self):
+        return self.connect().cursor()
 
-# Define a SQL query to create a table
-create_table_query = '''
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL
-);
-'''
+    def dbInitialize():
+        # Connect to SQLite database (this will create a new database if it doesn't exist)
+        conn = sqlite3.connect('starcraft.db')
 
-create_table_query2 = '''
-CREATE TABLE IF NOT EXISTS users (
-    username TEXT PRIMARY KEY,
-    password TEXT
-);
-'''
+        # Create a cursor object to execute SQL queries
+        cursor = conn.cursor()
 
-create_table_gate = '''
-CREATE TABLE IF NOT EXISTS game
-'''
+        # Define a SQL query to create a table
+        create_table_query = '''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL
+        );
+        '''
+
+        create_table_query2 = '''
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            password TEXT
+        );
+        '''
+
+        cursor.execute("INSERT INTO users (username,password) VALUES (?,?)",("user1","pass"))
+        cursor.execute("INSERT INTO users (username,password) VALUES (?,?)",("user2","pass"))
 
 
-# Execute the query to create the table
-cursor.execute(create_table_query2)
+        # Execute the query to create the table
+        cursor.execute(create_table_query2)
 
-# Commit the changes and close the connection
-conn.commit()
-conn.close()
+        # Commit the changes and close the connection
+        conn.commit()
+        conn.close()
